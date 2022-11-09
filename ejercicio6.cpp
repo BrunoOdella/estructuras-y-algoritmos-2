f1b6f1a70150964d1540082d91055fb5f4e73a1f
Matias Olivera <matiasoliverapimienta@hotmail.com>
2022-11-09 19:38:23 -0300

    areglamos ej6

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -2,47 +2,50 @@
 #include "tads/arrays.cpp"
 using namespace std;
 
+
+
+int numeroSinPareja(int*& arreglo, int desde, int hasta){
+    
+    if (desde==hasta){
+        return arreglo[hasta];
+    }
+    if (((desde + hasta)/2)%2 != 0 ){
+        if (arreglo[(desde + hasta)/2] == arreglo[((desde + hasta)/2)-1]){
+            numeroSinPareja(arreglo,((desde + hasta)/2)+1,hasta);
+
+        }else {
+             numeroSinPareja(arreglo,desde,((desde + hasta)/2)-1);
+        }
+       
+    }else {
+        if (arreglo[(desde + hasta)/2] == arreglo[((desde + hasta)/2)+1] ){
+            numeroSinPareja(arreglo,((desde + hasta)/2)+2,hasta);
+        }else{
+            numeroSinPareja(arreglo,desde,((desde + hasta)/2));
+        }
+    }
+
+
+    
+}
 int main(){
     int cantElem, valor;
     cin >> cantElem;
-    int* arreglo=new int[cantElem]();
+    int* arreglo=new int[cantElem];
     for (int i = 0; i < cantElem; i++)
     {
         cin >> valor;
         arreglo[i]=valor;
     }
+   
+    cout << numeroSinPareja(arreglo,0,cantElem-1) << endl;
+    delete [] arreglo;
+    return 0;
 
 }
-
-int numeroSinPareja(int*& arreglo, int cantElem, int pos){
-
-    if ((!arreglo[pos+1] || !arreglo[pos-1]) || (arreglo[pos]!=arreglo[pos+1] && arreglo[pos]!=arreglo[pos-1]))
-    {
-        return arreglo[pos];
-    }
-    //COSTADOS PARES?
-    if((cantElem-1/2)%2==0){
-        if (arreglo[pos-1]!= arreglo[pos])
-        {
-            numeroSinPareja(arreglo,cantElem+1/2,pos+pos/2);
-        }else
-        {
-            numeroSinPareja(arreglo,cantElem+1/2,pos-pos/2);
-        }     
-    }else
-    {
-    //COSTADOS IMPARES
-        if (arreglo[pos-1]!= arreglo[pos])
-        {
-            numeroSinPareja(arreglo,cantElem-1/2,pos-pos/2-1);
-        }else
-        {
-            numeroSinPareja(arreglo,cantElem-1/2,pos+pos/2+1);
-        } 
-    }
     
     
 
 
     
-}
+
