2e6f6b88d585bea5ff39cad2364093f9d8c05bdf
Matias Olivera <matiasoliverapimienta@hotmail.com>
2022-11-12 15:37:36 -0300

    arreglado ej1 problema de overflow

--- tads/hashCerrado.cpp
+++ tads/hashCerrado.cpp
@@ -1,96 +1,105 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 typedef struct _RepresentacionTablaHashCerrado *TablaHashCerrado;
 
-struct _RepresentacionTablaHashCerrado{
-	string* tabla;
+struct _RepresentacionTablaHashCerrado
+{
+	string *tabla;
 	int cantElem;
 	int largoVec;
 };
 
-int miHash(int largo,string pal){
-	int x=31;
-	int ret=0;
-	for (int i=0; i<pal.length();i++){
-		if (ret<2^10){
-		ret=x*ret + pal[i];
-		}else{
-			ret=ret%largo;
+// FUNCION DE HORNER SACADA DE INTERNET-ROBEDWARDS EN YOUTUBE
+long long horner(string pal)
+{
+	int x = 31;
+
+	long long ret = 0;
+	for (int i = 0; i < pal.length(); i++)
+	{
+		if (pal[i] != '@' && pal[i] != '.')
+		{
+			ret = x * ret + (int)pal[i];
+		}
+		if (ret < 0)
+		{
+			ret = ret + 9223372036854775807;
 		}
 	}
-	return ret%largo;
-	//return n%largo;
-}
-//FUNCION DE HORNER SACADA DE INTERNET-ROBEDWARDS EN YOUTUBE
-long long horner (string pal){
-	int x=31;
-	long long ret=0;
-	for (int i=0; i<pal.length();i++){
-		ret=x*ret + pal[i];
-	}
+
 	return ret;
 }
-//PRE: RECIBE UN NUMERO
-// POS : DEVUEVLO TRUE SI ES PRIMO 
-bool esPrimoo(int n) {
+int miHash(int largo, string pal)
+{
 
-	if (n <= 1) {
+	long long ret = horner(pal);
+	ret = ret % largo;
+	return ret;
+	
+}
+
+// PRE: RECIBE UN NUMERO
+//  POS : DEVUEVLO TRUE SI ES PRIMO
+bool esPrimo(int num)
+{
+	if (num < 2 || (num % 2 == 0 && num > 2))
+	{
 		return false;
 	}
-	for (int i = 2; i < n; i++) {
-		if (n % i == 0) {
+	for (int i = 3; i < num / 2; i += 2)
+	{
+		if (num % i == 0)
+		{
 			return false;
 		}
 	}
 	return true;
 }
 
-
-//PRE: RECIBE UN NUMERO
-// POS : DEVUEVLO EL PRIMO MAYOR  MAS PROXIMO A EL 
-int proximoPrimo(int n) {
-	int ret = n;
-	bool encontre = false;
-	for (int i = n; !encontre; i++) {
-		if (esPrimoo(i)) {
-			ret = i;
-			encontre = true;
-
-
-		}
-	}
-	return ret;
+int proximoPrimo(int num)
+{
+	// for(num=num+1; !esPrimo(num); num++);
+	while (!esPrimo(++num))
+		;
+	return num;
 }
 
-
-TablaHashCerrado crearTabla (int n){
-	int tamano=proximoPrimo(n*2);
-    TablaHashCerrado nueva = new _RepresentacionTablaHashCerrado;
-    nueva->tabla= new string[tamano];
-	nueva->largoVec=tamano;
-	nueva->cantElem=0;
+TablaHashCerrado crearTabla(int n)
+{
+	int tamano = proximoPrimo(n * 2);
+	TablaHashCerrado nueva = new _RepresentacionTablaHashCerrado;
+	nueva->tabla = new string[tamano];
+	nueva->largoVec = tamano;
+	nueva->cantElem = 0;
 	return nueva;
 }
 
-void agregar(TablaHashCerrado &t, string correo){
-	int pos=miHash(t->largoVec,correo);
-	while (t->tabla[pos]!=""){
-		if (t->tabla[pos].compare(correo)==0){
-			
+void agregar(TablaHashCerrado &t, string correo)
+{
+	int pos = miHash(t->largoVec, correo);
+
+	while (t->tabla[pos] != "")
+	{
+		if (t->tabla[pos].compare(correo) == 0)
+		{
+
 			return;
-		}else{
-			pos=((pos+1)%t->largoVec);
+		}
+		else
+		{
+			pos = ((pos + 1) % t->largoVec);
 		}
 	}
-	
-	t->tabla[pos]=correo;
-	t->cantElem++;
 
+	t->tabla[pos] = correo;
+	t->cantElem++;
 }
-int correosUnicos(TablaHashCerrado t){
+int correosUnicos(TablaHashCerrado t)
+{
 	return t->cantElem;
 }
 /*int main (){
@@ -105,9 +114,3 @@ int correosUnicos(TablaHashCerrado t){
 
 }
 */
-
-
-
-
-
-
